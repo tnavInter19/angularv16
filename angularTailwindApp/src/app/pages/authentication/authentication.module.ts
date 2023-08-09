ecfeb43dc8cf446936d3272dce9c7f1857c5a283
tnav97 <naveen.t@interspark.com>
2023-08-09 10:31:21 +0530

    new changes

--- angularTailwindApp/src/app/pages/authentication/authentication.module.ts
+++ angularTailwindApp/src/app/pages/authentication/authentication.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    AuthenticationComponent
+    AuthenticationComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
